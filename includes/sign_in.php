77e914f63ac3dba99ea33008c4e338b137b4bc84
sqwizXhar <sonagalkina127@gmail.com>
2024-11-05 20:23:34 +0300

    changed

--- includes/sign_in.php
+++ includes/sign_in.php
@@ -1,32 +1,52 @@
 <?php
-    session_start();
-    require_once 'db_connect.php';
+    require_once '../includes/db_connect.php';
+    require_once '../helpers/validation_helpers.php';
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $username = $_POST['username'];
-        $password = md5($_POST['password']);
+        $username = clear_data($_POST['username']);
+        $password = md5(clear_data($_POST['password']));
 
-        $found_user = 'SELECT * FROM user_data WHERE username = :username AND password = :password';
-        $stmt = $pdo->prepare($found_user);
-        $stmt->execute([
-            'username' => $username,
-            'password' => $password
+        set_form_data([
+            'username' => $username
         ]);
 
-        if($stmt->rowCount() > 0){
-            $user = $stmt->fetch(PDO::FETCH_ASSOC);
+        $check_user_query = 'SELECT * FROM user_data WHERE username = :username';
+        $stmt = $pdo->prepare($check_user_query);
+        $stmt->execute([
+            ':username' => $username]
+        );
 
-            $_SESSION['user'] = [
-                'id' => $user['id'],
-                'username' => $user['username'],
-                'email' => $user['email'],
-                'profile_picture' => $user['profile_picture'],
-            ];
+        if ($stmt->rowCount() > 0) {
+            $check_password_query = 'SELECT * FROM user_data WHERE username = :username AND password = :password';
+            $stmt = $pdo->prepare($check_password_query);
+            $stmt->execute([
+                'username' => $username,
+                'password' => $password
+            ]);
 
-            header('Location: ../home.php');
+            if ($stmt->rowCount() > 0) {
+                $user = $stmt->fetch(PDO::FETCH_ASSOC);
+                $_SESSION['user'] = [
+                    'id' => $user['id'],
+                    'username' => $user['username'],
+                    'email' => $user['email'],
+                    'profile_picture' => $user['profile_picture'],
+                ];
+                clear_form_data();
+                header('Location: ../home.php');
+                exit();
+            } else {
+                $_SESSION['message'] = 'Invalid password';
+                $_SESSION['form_data']['password'] = '';
+                header('Location: ../index.php');
+                exit();
+            }
+        } else {
+            $_SESSION['message'] = 'User does not exist';
+            clear_form_data();
+            header('Location: ../index.php');
+            exit();
         }
-        else{
-            header("location: ../index.php");
-        }
-}
+    }
+?>
--- includes/sign_up.php
+++ includes/sign_up.php
@@ -1,20 +1,35 @@
 <?php
-    session_start();
-    require_once 'db_connect.php';
+    require_once '../includes/db_connect.php';
+    require_once '../helpers/validation_helpers.php';
 
-    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-        $username = $_POST['username'];
-        $email = $_POST['email'];
-        $password = $_POST['password'];
-        $password_confirm = $_POST['password_confirm'];
+        $username = clear_data($_POST['username']);
+        $email = clear_data($_POST['email']);
+        $password = clear_data($_POST['password']);
+        $password_confirm = clear_data($_POST['password_confirm']);
 
-        if ($password === $password_confirm) {
+        set_form_data([
+            'username' => $username,
+            'email' => $email
+        ]);
+
+        if (check_password_match($password, $password_confirm)) {
+
+            $check_user_query = 'SELECT * FROM user_data WHERE username = :username';
+            $stmt = $pdo->prepare($check_user_query);
+            $stmt->execute([':username' => $username]);
+
+            if ($stmt->rowCount() > 0) {
+                $_SESSION['message'] = 'Username already taken';
+                header('Location: ../register.php');
+                exit();
+            }
 
             $path = 'uploads/' . time() . $_FILES['profile_picture']['name'];
             if (!move_uploaded_file($_FILES['profile_picture']['tmp_name'], '../' . $path)) {
-                $_SESSION['message'] = 'error loading file';
-                header('location: ../register.php');
+                $_SESSION['message'] = 'Error loading file';
+                header('Location: ../register.php');
                 exit();
             }
 
@@ -25,20 +40,19 @@
             $stmt->execute([
                 ':username' => $username,
                 ':email' => $email,
-                'profile_picture' => $path,
+                ':profile_picture' => $path,
                 ':password' => $password
             ]);
 
+            $_SESSION['username_for_login'] = $username;
             $_SESSION['message'] = 'Registration was successful!';
-            header('location: ../index.php');
+            clear_form_data();
+            header('Location: ../index.php');
             exit();
-
         } else {
-            $_SESSION["message"] = 'Passwords do not match!';
+            $_SESSION['message'] = 'Passwords do not match!';
             header('Location: ../register.php');
             exit();
         }
     }
 ?>
-
-
